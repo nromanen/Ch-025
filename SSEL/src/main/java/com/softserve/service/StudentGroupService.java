0c5c89e737f44edb8f4b252181006a61e5b73d5a
romahomyshyn <roma.homyshyn@gmail.com>
2014-10-20 16:36:19 +0300

    add service

---
+++ SSEL/src/main/java/com/softserve/service/StudentGroupService.java
@@ -0,0 +1,19 @@
+package com.softserve.service;
+
+import java.util.List;
+
+import com.softserve.entity.StudentGroup;
+
+public interface StudentGroupService {
+	public void addStudentGroup(StudentGroup studentGroup);
+
+	public void updateStudentGroup(StudentGroup studentGroup);
+
+	public void deleteStudentGroup(StudentGroup studentGroup);
+
+	public StudentGroup getStudentGroupById(int id);
+
+	public List<StudentGroup> getStudentGroupsByGroupNumber(int number);
+
+	public List<StudentGroup> getAllStudentGroups();
+}
