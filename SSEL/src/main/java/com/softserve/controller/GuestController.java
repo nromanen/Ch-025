039fd324971c76885b8ea9b6eac719803eb346b6
romahomyshyn <roma.homyshyn@gmail.com>
2014-10-23 19:11:25 +0300

    Security updates

--- SSEL/src/main/java/com/softserve/controller/GuestController.java
+++ SSEL/src/main/java/com/softserve/controller/GuestController.java
@@ -1,6 +1,10 @@
- package com.softserve.controller;
- import java.util.List;
+package com.softserve.controller;
+
+import java.security.Principal;
+import java.util.List;
 import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +19,7 @@ import com.softserve.entity.Category;
 import com.softserve.entity.CourseScheduler;
 import com.softserve.entity.StudentGroup;
 import com.softserve.entity.Subject;
+import com.softserve.entity.User;
 import com.softserve.service.CategoryService;
 import com.softserve.service.CourseSchedulerService;
 import com.softserve.service.RoleService;
@@ -25,33 +30,42 @@ import com.softserve.service.UserService;
 
 @Controller
 public class GuestController {
-	
+
 	private static final Logger LOG = LoggerFactory
 			.getLogger(GuestController.class);
 
 	@Autowired
 	private SubjectService subjectService;
-	
+
 	@Autowired
 	private CategoryService categoryService;
-	
+
 	@Autowired
 	private CourseSchedulerService cSchedulerService;
-	
+
 	@Autowired
 	private StudentGroupService studentGroupService;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private RoleService roleService;
-	
+
 	@Autowired
 	private StudentCabinetSevice studCabService;
-	
+
+	private User user;
+
+	@RequestMapping(value = "/enter")
+	public String enter(Model model, Principal principal) {
+		LOG.info("User login {}", principal.getName());
+		user = userService.getUserByEmail(principal.getName());
+		return "index";
+	}
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Model model) {	
+	public String index(Model model) {
 		LOG.debug("Visit index page as guest");
 		Set<Subject> subjects = subjectService.getAllSubjects();
 		Set<Category> categories = categoryService.getAllCategories();
@@ -59,7 +73,7 @@ public class GuestController {
 		model.addAttribute("catList", categories);
 		return "index";
 	}
-	
+
 	@RequestMapping(value = "/course", method = RequestMethod.GET)
 	public String course(@RequestParam Integer subjectId, Model model) {
 		LOG.debug("Visit course page as guest");
@@ -68,14 +82,15 @@ public class GuestController {
 		model.addAttribute("subList", subjects);
 		model.addAttribute("catList", categories);
 		Subject subject = subjectService.getSubjectById(subjectId);
-		List<CourseScheduler> schedule = 
-				cSchedulerService.getCourseScheduleresBySubjectId(subject.getId());
-		StudentGroup row = studCabService.getStudentGroupByUserAndCourseId(1, schedule.get(0).getId());
-		boolean isSubscribe = row == null; 
+		List<CourseScheduler> schedule = cSchedulerService
+				.getCourseScheduleresBySubjectId(subject.getId());
+		StudentGroup row = studCabService.getStudentGroupByUserAndCourseId(1,
+				schedule.get(0).getId());
+		boolean isSubscribe = row == null;
 		model.addAttribute("isSubscribe", isSubscribe);
 		model.addAttribute("schedule", schedule.get(0));
 		model.addAttribute("subject", subject);
 		return "course";
 	}
-	
+
 }
