39031a66430dc4dde8e84eb7d748b7b6cb7cf3cc
vetallp <vitaliy.paskar@gmail.com>
2014-10-23 12:49:35 +0300

    adding and changing some files to enable logging in database

---
+++ SSEL/src/main/java/com/softserve/connectionFactory/ConnectionFactory.java
@@ -0,0 +1,38 @@
+package com.softserve.connectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
+ 
+public class ConnectionFactory {
+    private static interface Singleton {
+        final ConnectionFactory INSTANCE = new ConnectionFactory();
+    }
+ 
+    private final DataSource dataSource;
+ 
+    private ConnectionFactory() {
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "1234"); // or get properties from some configuration file
+      
+        GenericObjectPool pool = new GenericObjectPool();
+        DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
+                "jdbc:mysql://localhost/ssel", properties
+        );
+        new PoolableConnectionFactory(
+                connectionFactory, pool, null, "SELECT 1", 3, false, false, Connection.TRANSACTION_READ_COMMITTED
+        );
+ 
+        this.dataSource = new PoolingDataSource(pool);
+    }
+ 
+    public static Connection getDatabaseConnection() throws SQLException {
+        return Singleton.INSTANCE.dataSource.getConnection();
+    }
+}
