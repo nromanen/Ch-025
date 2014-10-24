bfe78cc8704ca548471eb5ecf02ebdd6d01394bb
hashcv <hash.cv@gmail.com>
2014-10-24 10:30:32 +0300

    Update RoleDao.java

--- SSEL/src/main/java/com/softserve/dao/RoleDao.java
+++ SSEL/src/main/java/com/softserve/dao/RoleDao.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.softserve.dao;
 
 import java.util.Set;
@@ -19,25 +18,3 @@ public interface RoleDao {
 	
 	public Role getRoleByName(String name);
 }
-=======
-package com.softserve.dao;
-
-import java.util.Set;
-
-import com.softserve.entity.Role;
-
-public interface RoleDao {
-
-	public Role getRoleById(int id);
-
-	public void addRole(Role role);
-
-	public void updateRole(Role role);
-
-	public void deleteRole(Role role);
-
-	public Set<Role> getAllRoles();
-	
-	public Role getRoleByName(String name);
-}
->>>>>>> cdb409cfb5a30fe58335a64c2c69471f7d040982
