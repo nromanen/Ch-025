840f0298eed113fb31598b207948a273b5a0a768
Vanuxa <ivan.khotynskyi@gmail.com>
2014-10-29 13:31:09 +0200

    change view categories

--- SSEL/src/main/java/com/softserve/controller/AdministratorController.java
+++ SSEL/src/main/java/com/softserve/controller/AdministratorController.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.softserve.entity.Category;
 import com.softserve.entity.CourseScheduler;
 import com.softserve.entity.Subject;
 import com.softserve.entity.User;
+import com.softserve.service.CategoryService;
 import com.softserve.service.CourseSchedulerService;
 import com.softserve.service.StudentGroupService;
 import com.softserve.service.SubjectService;
@@ -36,6 +39,9 @@ public class AdministratorController {
 	
 	@Autowired
 	private CourseSchedulerService courceSchedulerService;
+	
+	@Autowired
+	private CategoryService categoryService;
 
 	@RequestMapping(value = "/administrator", method = RequestMethod.GET)
 	public String administrator(Model model) {
@@ -52,8 +58,38 @@ public class AdministratorController {
 	@RequestMapping(value = "/viewAllCategories", method = RequestMethod.GET)
 	public String viewAllCategories(Model model) {
 		LOG.debug("Visit viewAllCategories page");
-		
+		Set<Category> categories = categoryService.getAllCategories();
+		model.addAttribute("categories", categories);
 		return "viewAllCategories";
+	}
+	
+	@RequestMapping(value = "/deleteCategory", method = RequestMethod.GET)
+	public String deleteCategory(
+			@RequestParam(value = "categoryId", required = false) Integer categoryId, Model model) {
+		LOG.debug("Visit viewAllCategories page");
+		if (categoryId != null) {
+		Category category = categoryService.getCategoryById(categoryId);
+		categoryService.deleteCategory(category);
+		}
+		Set<Category> categories =categoryService.getAllCategories();
+//		List<Category> list = new ArrayList<Category>(categories);
+//		Collections.sort(list,Collections.reverseOrder());
+		model.addAttribute("categories", categories);
+		return "redirect:/viewAllCategories";
+	}
+	
+	@RequestMapping(value = "/addCategory", method = RequestMethod.GET)
+	public String viewAllCategories(
+			@RequestParam(value = "category", required = false) String category, Model model) {
+		LOG.debug("Visit viewAllCategories page");
+		if (category != "" && category != null ) {
+		Category newCategory = new Category();
+		newCategory.setName(category);
+		categoryService.addCategory(newCategory);
+		Set<Category> categories =categoryService.getAllCategories();
+		model.addAttribute("categories", categories);
+		}
+		return "redirect:/viewAllCategories";
 	}
 	
 	@RequestMapping(value = "/viewAllUsers", method = RequestMethod.GET)
--- SSEL/src/main/webapp/WEB-INF/views/pages/viewAllCategories.jsp
+++ SSEL/src/main/webapp/WEB-INF/views/pages/viewAllCategories.jsp
@@ -6,6 +6,31 @@
 
 <div class="row">
 	<div class="col-lg-12">
-		<h1 align="center">This will add some categories!</h1>
+		<div class="col-md-3">
+		<br>
+		<form role="form" action="addCategory" align="center">
+		<div class="form-group">
+    <label for="addNewCategory">Add new category</label>
+	<input type="text" class="form-control" id="addNewCategory" name="category" placeholder="Input category">
+	</div>
+	<button type="submit" class="btn btn-primary btn-sm btn-block">Add category</button>
+	</form>
+		</div>
+		<div class="col-md-6">
+		<h1 align="center">Categories:</h1>
+		<table class="table table-bordered" align="center">
+		<tr align="center" class="info">
+		<td class="col-md-5">Category name</td>
+		<td class="col-md-1">Delete</td>
+		</tr>
+		<c:forEach items="${categories}" var="category">
+		<tr>
+		<td>${category.name}</td>
+		<td align="center"><a href="deleteCategory?categoryId=${category.id}">X</a></td>
+		</tr>
+		</c:forEach>
+		</table>
+		</div>
+		<div class="col-md-3"></div>
 	</div>
 </div>
