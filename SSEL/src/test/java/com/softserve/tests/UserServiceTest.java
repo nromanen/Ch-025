c5b4414ebfb2e21875db8b1034cd94cb9bd91fec
romahomyshyn <roma.homyshyn@gmail.com>
2014-11-25 09:56:20 +0200

    Few tests for user service

--- SSEL/src/test/java/com/softserve/tests/UserServiceTest.java
+++ SSEL/src/test/java/com/softserve/tests/UserServiceTest.java
@@ -1,63 +1,49 @@
 package com.softserve.tests;
 
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Date;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.softserve.dao.RoleDao;
-import com.softserve.dao.UserDao;
-import com.softserve.entity.Role;
-import com.softserve.entity.User;
-import com.softserve.entity.User.Social;
-import com.softserve.service.MailService;
-import com.softserve.service.TeacherRequestService;
-import com.softserve.service.impl.UserServiceImpl;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.softserve.service.UserService;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/spring/fortest/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/fortest/servlet-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/fortest/data.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DbUnitTestExecutionListener.class })
 public class UserServiceTest {
-	@Mock
-	private UserDao userDao;
-	@Mock
-	private RoleDao roleDao;
-	@Mock
-	private MailService mailService;
-	@Mock
-	private TeacherRequestService teacherRequestService;
 
-	@InjectMocks
-	private UserServiceImpl userServiceImpl;
-
-	@Before
-	public void setUp() {
-		MockitoAnnotations.initMocks(this);
-	}
+	@Autowired
+	private UserService userService;
 
 	@Test
-	public void testIsExistEmail() {
-		Role role = new Role();
-		role.setId(1);
-		role.setRole("Admin");
-		User user = new User();
-		user.setRole(role);
-		user.setEmail("roma1sk@mail.com");
-		user.setFirstName("Roma");
-		user.setLastName("Roma");
-		user.setBlocked(false);
-		user.setExpired(new Date());
-		user.setId(1);
-		user.setPassword("sagafsgf");
-		user.setRegistration(new Date());
-		user.setSocial(Social.FACEBOOK);
-		userServiceImpl.addUser(user);
-		when(userServiceImpl.getUserByEmail("roma1sk@mail.com")).thenReturn(
-				user);
+	@DatabaseSetup("classpath:users.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:users.xml")
+	public void testGetUserByEmail() {
+		assertNotNull(userService.getUserByEmail("roma1sk@mail.ru"));
+		assertNull(userService.getUserByEmail("roma@mail.ru"));
+	}
+	
+	@Test
+	@DatabaseSetup("classpath:users.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:users.xml")
+	public void testIsExistsUsers() {
+		assertTrue(userService.isExist("roma1sk@mail.ru"));
+		assertFalse(userService.isExist("roma@mail.ru"));
 	}
 }
