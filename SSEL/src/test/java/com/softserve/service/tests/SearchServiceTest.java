ed1ae9ee993d6b7c7c4ec7e8c2901131c4278c1a
andryshutka <andrshutka.as@gmail.com>
2014-11-25 22:33:28 +0200

    write searchService tests

--- SSEL/src/test/java/com/softserve/service/tests/SearchServiceTest.java
+++ SSEL/src/test/java/com/softserve/service/tests/SearchServiceTest.java
@@ -18,6 +18,7 @@ import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import com.softserve.entity.Category;
+import com.softserve.entity.Subject;
 import com.softserve.service.SearchService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -44,6 +45,14 @@ public class SearchServiceTest {
 	@Test
 	@DatabaseSetup("classpath:categories.xml")
 	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:categories.xml")
+	public void testSearchCategoriesZero() {
+		List<Category> listActual = searchService.getCategoriesByNamePart("Gfd");
+		assertEquals(0, listActual.size());
+	}
+	
+	@Test
+	@DatabaseSetup("classpath:categories.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:categories.xml")
 	public void testSearchCategoriesCount() {
 		Long listSize = searchService.getCategoriesQuantityByNamePart("a");
 		assertEquals(4l, listSize.longValue());
@@ -56,5 +65,54 @@ public class SearchServiceTest {
 		Long listSize = searchService.getCategoriesQuantityByNamePart("gsfs");
 		assertEquals(0l, listSize.longValue());
 	}
-
+	
+	@Test
+	@DatabaseSetup("classpath:subjects.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:subjects.xml")
+	public void testSearchSubjects() {
+		List<Subject> listActual = searchService.getSubjectsByNamePart("ma", 1, 10, "name", true);
+		assertEquals("Mathematics", listActual.get(0).getName());
+		assertEquals(1, listActual.size());
+	}
+	@Test
+	@DatabaseSetup("classpath:subjects.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:subjects.xml")
+	public void testSearchSubjectsPageSize() {
+		List<Subject> listActual = searchService.getSubjectsByNamePart("ja", 1, 3, "name", true);
+		assertEquals(3, listActual.size());
+	}
+	
+	@Test
+	@DatabaseSetup("classpath:subjects.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:subjects.xml")
+	public void testSearchSubjectsPages() {
+		List<Subject> listActual = searchService.getSubjectsByNamePart("ja", 2, 10, "name", true);
+		assertEquals(0, listActual.size());
+	}
+	
+	@Test
+	@DatabaseSetup("classpath:subjects.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:subjects.xml")
+	public void testSearchManySubjects() {
+		List<Subject> listActual = searchService.getSubjectsByNamePart("ja", 1, 10, "category", false);
+		assertEquals("Java EE", listActual.get(0).getName());
+		assertEquals(5, listActual.size());
+	}
+	
+	@Test
+	@DatabaseSetup("classpath:subjects.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:subjects.xml")
+	public void testSearchSubjectsCount() {
+		Long size = searchService.getSubjectsQuantityByNamePart("ja");
+		assertEquals(5l, size.longValue());
+	}
+	
+	@Test
+	@DatabaseSetup("classpath:subjects.xml")
+	@DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = "classpath:subjects.xml")
+	public void testSearchSubjectsCountZero() {
+		Long size = searchService.getSubjectsQuantityByNamePart("fd");
+		assertEquals(0, size.longValue());
+	}
+	
 }
