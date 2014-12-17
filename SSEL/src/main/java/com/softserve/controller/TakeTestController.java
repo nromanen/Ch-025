92d8a84b45c3f5cf18e7dcb7428389bde4ecfbde
Hashcv <hash.cv@gmail.com>
2014-12-17 09:35:40 +0200

    diagram

--- SSEL/src/main/java/com/softserve/controller/TakeTestController.java
+++ SSEL/src/main/java/com/softserve/controller/TakeTestController.java
@@ -1,30 +1,20 @@
 package com.softserve.controller;
 
-import java.beans.PropertyEditorSupport;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.softserve.entity.Block;
 import com.softserve.entity.Option;
 import com.softserve.entity.Question;
-import com.softserve.entity.QuestionText;
 import com.softserve.entity.Test;
 import com.softserve.entity.TestStatistic;
-import com.softserve.form.QuestionForm;
 import com.softserve.service.BlockService;
 import com.softserve.service.GroupService;
 import com.softserve.service.QuestionService;
@@ -50,13 +40,13 @@ public class TakeTestController {
 
 	@Autowired
 	private QuestionFormValidator questionFormValidator;
-	
+
 	@Resource(name = "TestStatisticService")
 	private TestStatisticService testStatisticService;
 
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private GroupService groupService;
 
@@ -71,20 +61,15 @@ public class TakeTestController {
 	 */
 	@RequestMapping(value = "/takeTest", method = RequestMethod.GET)
 	public String takeTest(@RequestParam(value = "testId", required = true) Integer testId, Model model) {
-		
-		Integer userId = userService.getUserByEmail(
-				userService.getCurrentUser()).getId();
-		List<TestStatistic> tsByUserByTestList = testStatisticService
-				.getTestStatisticByUserByTest(userId, testId);
-		float totalUserResult = ((float) testStatisticService
-				.getUserResultByTest(userId, testId) * 100);
+
+		Integer userId = userService.getUserByEmail(userService.getCurrentUser()).getId();
+		List<TestStatistic> tsByUserByTestList = testStatisticService.getTestStatisticByUserByTest(userId, testId);
+		float totalUserResult = ((float) testStatisticService.getUserResultByTest(userId, testId) * 100);
 
 		model.addAttribute("totalUserResult", totalUserResult);
 		model.addAttribute("tsByUserByTestList", tsByUserByTestList);
 		model.addAttribute("user", userService.getUserById(userId));
-		
-		
-		
+
 		Test test = testService.getTestById(testId);
 		List<Question> questions = questionService.getQuestionsByTestId(test.getId());
 
@@ -92,258 +77,44 @@ public class TakeTestController {
 		model.addAttribute("questions", questions);
 		return "takeTest";
 	}
-	
-	@RequestMapping(value = "/submitTest", method = RequestMethod.POST)
-	public String submitTest(
-			@RequestParam(value = "testId", required = true) Integer testId,
-			@RequestParam(value = "questionId", required = true) Integer questionId,
-			@RequestParam(value = "choices[]", required = false) ArrayList<Integer> choices,
-			Model model) {
-		
-		Integer userId = userService.getUserByEmail(
-				userService.getCurrentUser()).getId();
-		
-		 TestStatistic testStatistic = new TestStatistic();
-		 testStatistic.setTest(testService.getTestById(testId));
-		 testStatistic.setQuestion(questionService.getQuestionById(questionId));
-		 testStatistic.setUser(userService.getUserById(userId));
-		 testStatistic.setUserAnswers(choices);
-		 testStatistic.setGroup(groupService.getGroupsByStudent(userId).get(0));
-		 
-		 ArrayList<Option> options  = new ArrayList<Option>();
-		 for (Integer i:choices) {
-			 Option option = new Option();
-			 if (i == 0) option.setIsCorrect(false);
-			 if (i == 1) option.setIsCorrect(true);
-			 options.add(option);
-		 }
-		 
-		 testStatistic.setUserResult((float) questionService.getUserMarkByQuestion(questionId, options)[0]);
-		 testStatistic.setMaxResult((int) questionService.getUserMarkByQuestion(questionId, options)[1]);
-		 
-		 testStatisticService.addTestStatistic(testStatistic);
-		 
-		
-		
-		
-		
 
-		List<TestStatistic> tsByUserByTestList = testStatisticService
-				.getTestStatisticByUserByTest(userId, testId);
-		float totalUserResult = ((float) testStatisticService
-				.getUserResultByTest(userId, testId) * 100);
+	@RequestMapping(value = "/submitTest", method = RequestMethod.POST)
+	public String submitTest(@RequestParam(value = "testId", required = true) Integer testId,
+			@RequestParam(value = "questionId", required = true) Integer questionId,
+			@RequestParam(value = "choices[]", required = false) ArrayList<Integer> choices, Model model) {
+
+		Integer userId = userService.getUserByEmail(userService.getCurrentUser()).getId();
+
+		TestStatistic testStatistic = new TestStatistic();
+		testStatistic.setTest(testService.getTestById(testId));
+		testStatistic.setQuestion(questionService.getQuestionById(questionId));
+		testStatistic.setUser(userService.getUserById(userId));
+		testStatistic.setUserAnswers(choices);
+		testStatistic.setGroup(groupService.getGroupsByStudent(userId).get(0));
+
+		ArrayList<Option> options = new ArrayList<Option>();
+		for (Integer i : choices) {
+			Option option = new Option();
+			if (i == 0)
+				option.setIsCorrect(false);
+			if (i == 1)
+				option.setIsCorrect(true);
+			options.add(option);
+		}
+
+		testStatistic.setUserResult((float) questionService.getUserMarkByQuestion(questionId, options)[0]);
+		testStatistic.setMaxResult((int) questionService.getUserMarkByQuestion(questionId, options)[1]);
+
+		testStatisticService.addTestStatistic(testStatistic);
+
+		List<TestStatistic> tsByUserByTestList = testStatisticService.getTestStatisticByUserByTest(userId, testId);
+		float totalUserResult = ((float) testStatisticService.getUserResultByTest(userId, testId) * 100);
 
 		model.addAttribute("totalUserResult", totalUserResult);
 		model.addAttribute("tsByUserByTestList", tsByUserByTestList);
 		model.addAttribute("user", userService.getUserById(userId));
-		
+
 		return "ok";
-	
+
 	}
-
-	/**
-	 * Binder for block
-	 * 
-	 * @param request
-	 * @param binder
-	 * @throws Exception
-	 */
-	
-/*	@InitBinder
-	public void blockInitBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
-		binder.registerCustomEditor(Block.class, "block", new PropertyEditorSupport() {
-			@Override
-			public void setAsText(String id) {
-				Block block = blockService.getBlockById(Integer.parseInt(id));
-				setValue(block);
-			}
-		});
-	}*/
-
-	/**
-	 * Binder for tests
-	 * 
-	 * @param request
-	 * @param binder
-	 * @throws Exception
-	 */
-/*	@InitBinder
-	public void testInitBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
-		binder.registerCustomEditor(Test.class, "test", new PropertyEditorSupport() {
-			@Override
-			public void setAsText(String id) {
-				Test test = testService.getTestById(Integer.parseInt(id));
-				setValue(test);
-			}
-		});
-	}
-
-	*//**
-	 * Binder for question
-	 * 
-	 * @param request
-	 * @param binder
-	 * @throws Exception
-	 *//*
-	@InitBinder
-	public void questionInitBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
-		binder.registerCustomEditor(Question.class, "question", new PropertyEditorSupport() {
-			@Override
-			public void setAsText(String id) {
-				Question question = questionService.getQuestionById(Integer.parseInt(id));
-				setValue(question);
-			}
-		});
-	}*/
-	/**
-	 * Handle edit/add test form
-	 * 
-	 * @param subjectId
-	 *            unique subject indetifier
-	 * @param testId
-	 *            unique test identifier. if missing - add test, else - edit
-	 *            existing test
-	 * @param model
-	 *            data model for view
-	 * @return logical name for view
-	 */
-	/*
-	 * @RequestMapping(value = "/editTest", method = RequestMethod.GET) public
-	 * String addTestRender(@RequestParam(value = "subjectId", required = true)
-	 * Integer subjectId,
-	 * 
-	 * @RequestParam(value = "testId", required = false) Integer testId, Model
-	 * model) { List<Block> blocks =
-	 * blockService.getBlocksBySubjectId(subjectId); Test test = (testId ==
-	 * null) ? new Test() : testService.getTestById(testId);
-	 * model.addAttribute("test", test); model.addAttribute("blocks", blocks);
-	 * model.addAttribute("subjectId", subjectId); model.addAttribute("op",
-	 * (testId == null) ? true : false); return "editTest"; }
-	 *//**
-	 * Validate and insert/update test
-	 * 
-	 * @param test
-	 *            test form
-	 * @param model
-	 *            data for view
-	 * @param result
-	 *            result binder
-	 * @return logical name of view
-	 */
-	/*
-	 * @RequestMapping(value = "/saveTest", method = RequestMethod.POST) public
-	 * String processTestSubmit(@ModelAttribute("test") Test test,
-	 * 
-	 * @RequestParam("op") Boolean op, Model model, BindingResult result) {
-	 * testValidator.validate(test, result); Block block =
-	 * blockService.getBlockById(test.getBlock().getId()); Integer subjectId =
-	 * block.getSubject().getId(); Integer testId = test.getId(); if
-	 * (result.hasErrors()) { List<Block> blocks =
-	 * blockService.getBlocksBySubjectId(subjectId); model.addAttribute("test",
-	 * test); model.addAttribute("blocks", blocks); model.addAttribute("error",
-	 * "has errors"); return
-	 * "redirect:editTest?subjectId="+subjectId+"&testId="+testId; } test = (op)
-	 * ? testService.addTest(test) : testService.updateTest(test); return
-	 * "redirect:tests?blockId="+block.getId()+"&subjectId="+subjectId; }
-	 *//**
-	 * Edit/add question
-	 * 
-	 * @param testId
-	 *            unique test identifier.
-	 * @param questionId
-	 *            unique question identifier. If missing add, else - edit
-	 * @param model
-	 *            data for view
-	 * @return logical name for view
-	 */
-	/*
-	 * @RequestMapping(value="/editQuestion", method = RequestMethod.GET) public
-	 * String addQuestionRender(@RequestParam(value = "testId", required = true)
-	 * Integer testId,
-	 * 
-	 * @RequestParam(value = "questionId", required = false) Integer questionId,
-	 * Model model) { Question question; List<Option> answers; QuestionForm
-	 * questionForm = new QuestionForm(); Test test =
-	 * testService.getTestById(testId); if (questionId == null) { question = new
-	 * Question(); question.setTest(test); question.setMark(0.0); answers = new
-	 * ArrayList<>(); for(int i=0; i < 4; i++) { Option ans = new Option();
-	 * ans.setIsCorrect(false); answers.add(ans); }
-	 * questionForm.setQuestion(question); questionForm.setName("");
-	 * questionForm.setAnswers(answers); model.addAttribute("op", true); } else
-	 * { question = questionService.getQuestionById(questionId);
-	 * questionForm.setQuestion(question);
-	 * questionForm.setName(question.getQuestion().getValue()); answers =
-	 * question.getQuestion().getOptions(); questionForm.setAnswers(answers);
-	 * model.addAttribute("op", false); } model.addAttribute("questionForm",
-	 * questionForm); model.addAttribute("testName", test.getName()); return
-	 * "editQuestion"; }
-	 *//**
-	 * Validate and insert/update question
-	 * 
-	 * @param form
-	 *            form with question and answers
-	 * @param result
-	 *            binder for form
-	 * @param model
-	 *            data for view
-	 * @return logical name for view
-	 */
-	/*
-	 * @RequestMapping(value="/saveQuestion", method = RequestMethod.POST)
-	 * public String processSubmitaddQuestion(@ModelAttribute QuestionForm form,
-	 * 
-	 * @RequestParam(value = "op", required = true) Boolean op, BindingResult
-	 * result, Model model) { int testId = form.getQuestion().getTest().getId();
-	 * int questionId = form.getQuestion().getId();
-	 * questionFormValidator.validate(form, result); if (result.hasErrors()) {
-	 * return "editQuestion?testId="+testId+"&questionId="+questionId; } Test
-	 * test = testService.getTestById(testId); form.getQuestion().setTest(test);
-	 * Question question = form.getQuestion(); QuestionText questionText = new
-	 * QuestionText(); questionText.setOptions(form.getAnswers());
-	 * questionText.setValue(form.getName());
-	 * question.setQuestionText(questionText);
-	 * question.setMark(question.getMark()); question = (op) ?
-	 * questionService.addQuestion(question) :
-	 * questionService.updateQuestion(question); return
-	 * "redirect:testInfo?testId="+testId; }
-	 *//**
-	 * Render test info page
-	 * 
-	 * @param testId
-	 *            unique test identifier
-	 * @param model
-	 *            data for view
-	 * @return logical name of view
-	 */
-	/*
-	 * @RequestMapping(value = "/testInfo", method = RequestMethod.GET) public
-	 * String printTestInfo(@RequestParam(value = "testId", required = true)
-	 * Integer testId, Model model) { Test test =
-	 * testService.getTestById(testId); List<Question> questions =
-	 * questionService.getQuestionsByTestId(testId); List<QuestionText> texts =
-	 * new ArrayList<>(); if (questions != null) { for (Question question :
-	 * questions) { texts.add(question.getQuestion()); } }
-	 * model.addAttribute("texts", texts); model.addAttribute("test", test);
-	 * model.addAttribute("questions", questions); return "testInfo"; }
-	 * 
-	 * @RequestMapping(value = "/deleteTest", method = RequestMethod.POST)
-	 * public String deleteTest(@RequestParam(value = "testId", required = true)
-	 * Integer testId,
-	 * 
-	 * @RequestParam(value = "subjectId", required = true) Integer subjectId,
-	 * 
-	 * @RequestParam(value = "blockId", required = true) Integer blockId) {
-	 * testService.deleteTest(testId); return
-	 * "redirect:tests?blockId="+blockId+"&subjectId="+subjectId; }
-	 * 
-	 * @RequestMapping(value = "/deleteQuestion", method = RequestMethod.POST)
-	 * public String deleteQuestion(@RequestParam(value = "questionId", required
-	 * = true) Integer questionId,
-	 * 
-	 * @RequestParam(value = "testId", required = true) Integer testId) {
-	 * Question question = new Question(); question.setId(questionId);
-	 * questionService.deleteQuestion(question); return
-	 * "redirect:testInfo?testId="+testId; }
-	 */
-
 }
--- SSEL/src/main/webapp/WEB-INF/views/pages/tests/takeTest.jsp
+++ SSEL/src/main/webapp/WEB-INF/views/pages/tests/takeTest.jsp
@@ -9,8 +9,9 @@
 <c:choose>
 	<c:when test="${!empty tsByUserByTestList}">
 		<br />
-		<h2 align="center">We're sorry, but is seems that you already
-			passed this test yet.</h2>
+		<h2 align="center">
+			<spring:message code="label.userTS.you_have_not_passed_this_test" />
+		</h2>
 	</c:when>
 	<c:otherwise>
 		<div class="row">
@@ -30,17 +31,11 @@
 							<tbody>
 								<c:forEach items="${questionList.getQuestion().getOptions()}"
 									var="option" varStatus="ind">
-
-
-
 									<tr class="success">
 										<td class="col-md-11">${option.value}</td>
 										<td class="col-md-1" align="center"><input
 											type="checkbox" id="selecta" /></td>
 									</tr>
-
-
-
 								</c:forEach>
 								<tr>
 									<td colspan="2"><i>Earned ${testStatistic.userResult}
@@ -67,37 +62,37 @@
 <script type="text/javascript">
 	$(document).ready(function() {
 		$("#submitTest").click(function() {
-		//	if (confirm('Are you sure?')) {
-				
-				$('table').each(function(j,ele){
-					testId0 = '${test.id}';
-					str = "";
-					var choises = new Array();
-					
-					$(this).find(':checkbox').each(function(i,el){
-				        if( $(el).prop('checked') ){
-				        	str = str + '1';
-				        	choises.push(1);
-				        } else {
-				        	str = str + '0';
-				        	choises.push(0);
-				        }
-				    });
-					//alert($(ele).attr('id') + ": " + str);
-					$.ajax({
-						type : "POST",
-						url : "submitTest",
-						data : { testId: testId0, questionId: $(ele).attr('id'), 'choices[]': choises },
-						success : function(msg) {
-							
-						}
-					});
-					
-					});
+			//	if (confirm('Are you sure?')) {
 
-				window.location.href = "userTestStatistic?testId=" + testId0;	
-			
+			$('table').each(function(j, ele) {
+				testId0 = '${test.id}';
+				str = "";
+				var choises = new Array();
+
+				$(this).find(':checkbox').each(function(i, el) {
+					if ($(el).prop('checked')) {
+						str = str + '1';
+						choises.push(1);
+					} else {
+						str = str + '0';
+						choises.push(0);
+					}
+				});
+				
+				$.ajax({
+					type : "POST",
+					url : "submitTest",
+					data : {
+						testId : testId0,
+						questionId : $(ele).attr('id'),
+						'choices[]' : choises
+					}
+				});
+
+			});
+
+			window.location.href = "userTestStatistic?testId=" + testId0;
+
 		});
 	});
-	
 </script>
